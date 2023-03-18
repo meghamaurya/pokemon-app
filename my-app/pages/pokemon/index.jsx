ab154a10b0ba543cde48b602a68a6a8fa2d12572
megha <meghamini40@gmail.com>
2023-03-18 16:37:11 +0530

    set image

--- my-app/pages/pokemon/index.jsx
+++ my-app/pages/pokemon/index.jsx
@@ -1,8 +1,8 @@
+import Link from "next/link";
+
 export async function getStaticProps() {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
-  console.log(response, "respom");
   const data = await response.json();
-  console.log(data.results, "res");
 
   return {
     props: {
@@ -13,15 +13,28 @@ export async function getStaticProps() {
 
 function PokemonList({ pokemon }) {
   console.log(pokemon, "data");
-
+  let i;
   return (
-    <div>
-      <h2>heading</h2>
-      {pokemon &&
-        pokemon.map((poke) => {
-          return <>{poke.name}</>;
-        })}
-    </div>
+    <>
+      <div>
+        <img
+          src={
+            "https://res.cloudinary.com/lmn/image/upload/c_limit,h_360,w_640/e_sharpen:100/f_auto,fl_lossy,q_auto/v1/gameskinnyc/a/6/0/a6078c-2fd9033b2be04c9b947d1aac6cdaef60-a2551.gif"
+          }
+          width={160}
+          height={40}
+        ></img>
+        {pokemon &&
+          pokemon.map((poke, i) => {
+            i++;
+            return (
+              <>
+                <Link href={`/detailsPage/${i}`}>{poke.name}</Link>
+              </>
+            );
+          })}
+      </div>
+    </>
   );
 }
 
