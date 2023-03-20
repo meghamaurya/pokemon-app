86ef67164d4c5094832792709c8613476e6c45b0
megha <meghamini40@gmail.com>
2023-03-20 22:37:01 +0530

    added styles

--- my-app/pages/pokemon/index.jsx
+++ my-app/pages/pokemon/index.jsx
@@ -1,4 +1,7 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
+import styles from "../../styles/Pokemon.module.css";
+import DetailsPage from "@/Components/detailsPage";
 
 export async function getStaticProps() {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100");
@@ -11,31 +14,37 @@ export async function getStaticProps() {
   };
 }
 
-function PokemonList({ pokemon }) {
-  console.log(pokemon, "data");
-  let i;
+function Pokemon({ pokemon }) {
+  const router = useRouter();
+  const activeId = router.query.id;
+
   return (
-    <>
-      <div>
+    <div className={styles.appContainer}>
+      <div className={styles.pokeList}>
         <img
-          src={
-            "https://res.cloudinary.com/lmn/image/upload/c_limit,h_360,w_640/e_sharpen:100/f_auto,fl_lossy,q_auto/v1/gameskinnyc/a/6/0/a6078c-2fd9033b2be04c9b947d1aac6cdaef60-a2551.gif"
-          }
-          width={160}
-          height={40}
-        ></img>
-        {pokemon &&
-          pokemon.map((poke, i) => {
-            i++;
-            return (
-              <>
-                <Link href={`/detailsPage/${i}`}>{poke.name}</Link>
-              </>
-            );
-          })}
+          src="https://res.cloudinary.com/lmn/image/upload/c_limit,h_360,w_640/e_sharpen:100/f_auto,fl_lossy,q_auto/v1/gameskinnyc/a/6/0/a6078c-2fd9033b2be04c9b947d1aac6cdaef60-a2551.gif"
+          className={styles.logo}
+        />
+        {pokemon.map((poke, i) => {
+          i++;
+          return (
+            <Link
+              key={i}
+              href={`/pokemon/?id=${i}`}
+              className={`${styles.pokeName} ${
+                i == activeId ? styles.active : ""
+              }`}
+            >
+              {poke.name}
+            </Link>
+          );
+        })}
       </div>
-    </>
+      <div className={styles.detailspage}>
+        <DetailsPage />
+      </div>
+    </div>
   );
 }
 
-export default PokemonList;
+export default Pokemon;
